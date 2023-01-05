3785afce59197eef9f7a1abca905311e4b838d2c
Bearwann <erwan.rozet@outlook.fr>
2023-01-05 17:01:46 +0100

    meta in models and link in navbar

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    title_tag = models.CharField(max_length=255, default="dev.py" )
+    title_tag = models.CharField(max_length=255, default="dev.py")
+    meta = models.CharField(max_length=255, default="SEO !")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     
