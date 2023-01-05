dbfceebdeb633b17789b3ccf50bc45dcc2e3289b
Bearwann <erwan.rozet@outlook.fr>
2023-01-05 16:41:37 +0100

    Creating add_post.html and view and url

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 # def home(requests):
@@ -13,3 +13,8 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name  = 'add_post.html'
+    fileds = '__all__'
