808b3b4a8c85c6d22764172979dcd2ba07f62e3a
Bearwann <erwan.rozet@outlook.fr>
2023-01-08 20:09:08 +0100

    delete_post creation

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 # def home(requests):
 #     return render(requests, 'home.html', {})
@@ -17,14 +18,19 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
-    form_class = PostForm
+    form_class = PostForm # cette ligne gère la mise ne forme du form, basculer sur fields pour retourner au basic form
     template_name  = 'add_post.html'
     # fields = '__all__'
     # fields = ['title, 'body'] ### Pour ajouter seulement certains fields
 
 class UpdatePostView(UpdateView):
     model = Post
-    form_class = PostForm
+    form_class = EditForm  # une autre class de Form
     template_name  = 'update_post.html'
     # fields = '__all__'
-    fields = ['title', 'title_tag', 'body'] 
+    # fields = ['title', 'title_tag', 'body'] 
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
