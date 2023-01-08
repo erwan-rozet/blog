9a020bd332a33cb27f9bbd600a4bb8f27a7417ba
Bearwann <erwan.rozet@outlook.fr>
2023-01-08 21:00:05 +0100

    Adding datetime

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -8,6 +9,7 @@ class Post(models.Model):
     meta = models.CharField(max_length=255, default="SEO !")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
     
 
     def __str__(self):
--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,8 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name  = 'home.html'
-
+    # ordering = ['-id'] # inverser l'ordre des publications en attendant datetime
+    ordering = ['-post_date']
 
 class ArticleDetailView(DetailView):
     model = Post
